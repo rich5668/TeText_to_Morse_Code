51fad02e1feacd3ef3126c992672ab32c470bd2f
Richard Marquardt <rich5668@gmail.com>
2022-01-17 11:27:13 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-', '!': '-.-.--',
+                   ',': '__..__', "'": ".----.",
+
+                   }
+
+input_text = input('Hello, please input the message you would like to convert to morse code: ').upper()
+
+morse_code_list = []
+
+for char in input_text:
+    if char != ' ':
+        morse_code_list.append(MORSE_CODE_DICT[char])
+    elif char == ' ':
+        morse_code_list.append("/")
+
+    morse_code_list.append(" ")
+
+morse_code_string = ''.join(morse_code_list)
+print(morse_code_string)
